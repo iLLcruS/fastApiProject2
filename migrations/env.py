ba8ecd5e620a123bb0123b35aab1fdf3349c36b1
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-08 20:17:06 +0200

    Added to env secret keys for JWT

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT, SECRET_KEY_JWT, SECRET_KEY_JWT_VERIFICATION_RESET
+from config import DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT
 from models.user import metadata
 
 # this is the Alembic Config object, which provides
@@ -18,8 +18,6 @@ config.set_section_option(section, "DB_PASS", DB_PASS)
 config.set_section_option(section, "DB_HOST", DB_HOST)
 config.set_section_option(section, "DB_NAME", DB_NAME)
 config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "SECRET_KEY_JWT", SECRET_KEY_JWT)
-config.set_section_option(section, "SECRET_KEY_JWT_VERIFICATION_RESET", SECRET_KEY_JWT_VERIFICATION_RESET)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
