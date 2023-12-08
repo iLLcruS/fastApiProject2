6454477c137745d02be6cfdbeadc745837f75817
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-09 01:37:48 +0200

    Added new engine

--- app/auth/database_con.py
+++ app/auth/database_con.py
@@ -5,7 +5,7 @@ from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Boolean, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy import Boolean, ForeignKey, Integer, String, DateTime, func, create_engine
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 from app.models.user import role
@@ -63,7 +63,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     )
 
 
-
+engine_sync = create_engine(DATABASE_URL)
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
