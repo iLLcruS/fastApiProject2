2214772c284f0de4979edb9ab8b098997a6903a5
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-08 12:12:54 +0200

    Added schemas.py for User

---
+++ auth/manager.py
@@ -0,0 +1,78 @@
+from typing import Optional
+
+from fastapi import Depends, Request
+from fastapi_mail import FastMail, MessageSchema
+from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
+
+from config import SECRET_KEY_JWT_VERIFICATION_RESET
+from auth.database_con import User, get_user_db
+
+SECRET = f"{SECRET_KEY_JWT_VERIFICATION_RESET}"
+
+mail_config = {
+    "MAIL_USERNAME": "rostovskii.1020@gmail.com",
+    "MAIL_PASSWORD": "Reubjeon512",
+    "MAIL_FROM": "rostovskii.1020@gmail.com",
+    "MAIL_PORT": 587,
+    "MAIL_SERVER": "smtp.gmail.com",
+    "MAIL_FROM_NAME": "Vlad from Trello",
+    "MAIL_TLS": True,
+    "MAIL_SSL": False,
+}
+
+mail = FastMail(mail_config)
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        subject = "Registration Successful"
+        body = f"Thank you for registering, {user.name}! Your email: {user.email}"
+
+        message = MessageSchema(
+            subject=subject,
+            recipients=[user.email],
+            body=body,
+            subtype="html"
+        )
+
+        try:
+            await mail.send_message(message)
+            print(f"Email sent to {user.email} for user {user.id} registration.")
+        except Exception as e:
+            print(f"Failed to send email. Error: {str(e)}")
+        print(f"User {user.id} has registered. Email was sent")
+
+    async def create(
+            self,
+            user_create: schemas.UC,
+            safe: bool = False,
+            request: Optional[Request] = None,
+    ) -> models.UP:
+
+        await self.validate_password(user_create.password, user_create)
+
+        existing_user = await self.user_db.get_by_email(user_create.email)
+        if existing_user is not None:
+            raise exceptions.UserAlreadyExists()
+
+        user_dict = (
+            user_create.create_update_dict()
+            if safe
+            else user_create.create_update_dict_superuser()
+        )
+        password = user_dict.pop("password")
+        user_dict["hashed_password"] = self.password_helper.hash(password)
+        user_dict["role_id"] = 1
+
+        created_user = await self.user_db.create(user_dict)
+
+        await self.on_after_register(created_user, request)
+
+        return created_user
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
---
+++ auth/schemas.py
@@ -0,0 +1,23 @@
+from fastapi_users import schemas
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    name: str
+    email: str
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+
+class UserCreate(schemas.BaseUserCreate):
+    name: str
+    email: str
+    username: str
+    password: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
