61637d65c7f0217d12adf254cf05a828a44e5e36
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-09 04:50:29 +0200

    Added logger for router tasks

--- app/router/tasks.py
+++ app/router/tasks.py
@@ -1,17 +1,14 @@
-import datetime
-from fastapi import APIRouter, Depends
-from fastapi import Request
+
+import jwt
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import RedirectResponse
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from pydantic import BaseModel
+from sqlalchemy import select, update, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.responses import RedirectResponse
-from sqlalchemy import select, update
-from app.auth.database_con import get_user_db, get_async_session
-import jwt
-from app.auth.database_con import engine
+from app.auth.database_con import get_user_db, get_async_session, engine
+from app.models.logger import logger
 from app.models.tasks import task
-from sqlalchemy import insert, delete
-
 from config import SECRET_KEY_JWT
 
 router = APIRouter(
@@ -21,31 +18,20 @@ router = APIRouter(
 )
 
 
-def decode_user(token: str):
-    """
-    :param token: jwt token
-    :return:
-    """
-    decoded_data = jwt.decode(jwt=token,
-                              key=f'{SECRET_KEY_JWT}',
-                              algorithms=["HS256"],
-                              audience="Trello Auth"
-                              )
-    return decoded_data
+class Task(BaseModel):
+    title: str
+    description: str
+    user_executor_id: int
 
 
-def get_user_id_from_token(request: Request) -> int:
-    cookie = request.cookies.get("trello")
-    user_data = decode_user(cookie)
-    return int(user_data['sub'])
+class UpdateTask(BaseModel):
+    title: str
+    description: str
+    user_executor_id: int
 
 
-async def query_execute(query, session: AsyncSession):
-    result = await session.execute(query)
-    await session.commit()
-    await session.close()
-    return result
-
+class TaskDeleteData(BaseModel):
+    task_id: int
 
 @router.get("")
 async def redirect_to_current_user_tasks(request: Request, user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
@@ -55,50 +41,38 @@ async def redirect_to_current_user_tasks(request: Request, user_db: SQLAlchemyUs
     return RedirectResponse(redirect_url)
 
 
-class Task(BaseModel):
-    title: str
-    description: str
-    user_executor_id: int
-    end_timestamp: datetime.datetime
-
-
-class UpdateTask(BaseModel):
-    title: str
-    description: str
-    user_executor_id: int
-    end_timestamp: datetime.datetime
-
-
-class TaskDeleteData(BaseModel):
-    task_id: int
-
-
 @router.post("/{user_name}/add")
 async def create_task(request: Request, user_name: str, task_data: Task,
                       user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
     user_id = get_user_id_from_token(request)
-    query = insert(task).values(
-        title=task_data.title,
-        description=task_data.description,
-        user_executor_id=task_data.user_executor_id,
-        end_timestamp=task_data.end_timestamp,
-        user_creator_id=user_id
-    )
+    user_email = await user_db.get(user_id)
+
     async with AsyncSession(engine) as session:
-        await query_execute(query, session)
-    return "ALL OK~!"
+        try:
+            query = insert(task).values(
+                title=task_data.title,
+                description=task_data.description,
+                user_executor_id=task_data.user_executor_id,
+                user_creator_id=user_id,
+            )
+            await query_execute(query, session)
+            await log_operation(session, "Created Task", user_id, f'{user_email.email}')
+            return f'Task with title - {task_data.title} was created'
+        except Exception as e:
+            await log_operation(session, f"Task Creation Failed: {str(e)}", user_id, f'{user_email.email}')
+            return "Error during task creation"
 
 
 @router.post("/{username}/edit/{task_id}")
 async def update_task(request: Request, user_name: str, task_id: int, task_data: UpdateTask,
                       user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
     user_id = get_user_id_from_token(request)
+    user_email = await user_db.get(user_id)
 
     task_update_data = {
         "title": task_data.title,
         "description": task_data.description,
         "user_executor_id": task_data.user_executor_id,
-        "end_timestamp": task_data.end_timestamp,
         "user_creator_id": user_id,
     }
 
@@ -107,19 +81,30 @@ async def update_task(request: Request, user_name: str, task_id: int, task_data:
     query = update(task).values(task_update_data).where(task_update_condition)
 
     async with AsyncSession(engine) as session:
-        await query_execute(query, session)
-    return "Date updated"
+        try:
+            await query_execute(query, session)
+            await log_operation(session, "Updated Task", user_id, f'{user_email.email}')
+            return f'Task with title - {task_data.title} was updated tp'
+        except Exception as e:
+            await log_operation(session, f"Task Updated Failed: {str(e)}", user_id, f'{user_email.email}')
+            return "Error during task updated"
 
 
 @router.delete("/{user_name}/erase/{task_id}")
-async def erase_task(request: Request, user_name: str,task_id: str,
-                     session: AsyncSession = Depends(get_async_session)):
+async def erase_task(request: Request, user_name: str, task_id: str,
+                     session: AsyncSession = Depends(get_async_session), user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    user_id = get_user_id_from_token(request)
+    user_email = await user_db.get(user_id)
+
     if id is not None:
         query = delete(task).where(task.c.id == int(task_id))
-        await query_execute(query, session)
-        return f'Successfully deleted task with id - {task_id}'
-    else:
-        return {"message": "Error: value title or id must be not empty"}
+        try:
+            await query_execute(query, session)
+            await log_operation(session, "Deleted Task", user_id, f'{user_email.email}')
+            return f'Task was deleted'
+        except Exception as e:
+            await log_operation(session, f"Task Deleted Failed: {str(e)}", user_id, f'{user_email.email}')
+            return "Error during task deleted"
 
 
 @router.get("/get")
