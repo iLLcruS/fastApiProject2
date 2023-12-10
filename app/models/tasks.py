df0312fbbc4dff1489fea707965e4cbe1c91e41c
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-10 13:45:17 +0200

    Added new Column for status

--- app/models/tasks.py
+++ app/models/tasks.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, JSON, Boolean, TIMESTAMP, func, DateTime
 
+from app.models.status import status
 from app.models.user import user
 
 
@@ -15,4 +16,5 @@ task = Table(
     Column("start_timestamp", TIMESTAMP, arbitrary_types_allowed=True, nullable=False),
     Column("user_creator_id", Integer, ForeignKey(user.c.id)),
     Column("user_executor_id", Integer, ForeignKey(user.c.id)),
+    Column("status_id", Integer, ForeignKey(status.c.id), nullable=True)
 )
