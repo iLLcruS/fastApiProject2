ddac0063e68daf74af68c5924f71627d4ba6ad21
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-11 17:52:45 +0200

    Added column for board table

--- app/models/board.py
+++ app/models/board.py
@@ -1,5 +1,6 @@
 import datetime as dt
-from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, JSON, Boolean, TIMESTAMP, func, DateTime
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, JSON, Boolean, TIMESTAMP, func, DateTime, \
+    ARRAY
 
 from app.models.user import user
 
@@ -9,5 +10,6 @@ board = Table(
     "board",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("title", String, nullable=False)
+    Column("title", String, nullable=False),
+    Column("allowed_user_ids", ARRAY(Integer))
 )
