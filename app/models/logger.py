20daeb3399fa1cd4f4c81811068438280c504d63
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-11 21:04:54 +0200

    Delete foreign key

--- app/models/logger.py
+++ app/models/logger.py
@@ -10,8 +10,8 @@ logger = Table(
     metadata,
     Column("log_id", Integer, primary_key=True),
     Column("log_subject", String, nullable=False),
-    Column("log_id_user", Integer, ForeignKey(user.c.id), nullable=False),
-    Column("log_email_user", String, ForeignKey(user.c.email), nullable=True),
+    Column("log_id_user", Integer, nullable=False),
+    Column("log_email_user", String, nullable=True),
     Column("log_time", DateTime(timezone=True), server_default=func.now(), default=dt.datetime.now().date(),
            nullable=False),
 )
