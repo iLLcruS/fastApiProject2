8004927691a0a4853388e1e2ea7f9614a6fadcbc
iLucruS <90245001+iLucruS@users.noreply.github.com>
2023-12-11 16:22:25 +0200

    Added super-links

--- app/router/tasks.py
+++ app/router/tasks.py
@@ -9,7 +9,7 @@ from app.auth.database_con import get_user_db, get_async_session, engine
 from app.core.custom_routers_func import get_user_id_from_token, query_execute, execute_task_operation
 from app.models.tasks import task
 from app.models.status import status as status_table
-from config import SECRET_KEY_JWT
+from sqlalchemy import or_, and_
 
 router = APIRouter(
 
@@ -95,14 +95,66 @@ async def erase_task(request: Request, user_name: str, task_id: str,
         return await execute_task_operation(request, user_id, query, success_message, user_db)
 
 
-@router.get("/get")
-async def get_tasks(task_title: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(task).where(task.c.title == task_title)
-    result = await session.execute(query)
+@router.get("/get/{task_title}")
+async def get_task(request: Request,
+                    task_title: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        user_id = get_user_id_from_token(request)
+    except Exception as e:
+        print(e)
+        return "403 FORBIDDEN"
+
+    query = select(task).where(and_(task.c.title == task_title, or_(task.c.user_executor_id == user_id,task.c.user_creator_id == user_id)))
+    result_raw = await session.execute(query)
+    result = result_raw.fetchone()
     await session.close()
-    return result.all()
+
+    if result is None:
+        return "404 Not found"
+
+    response = []
+
+    column_names = ["id", "title", "description", "start_timestamp",
+                    "user_creator_id", "user_executor_id", "status_id",
+                    "board_id", "parent_task_id"]
+
+    if result is not None:
+        data = {column_names[i]: result[i] for i in range(len(column_names))}
+        response.append(data)
+
+    return response
 
 
+@router.get("/all")
+async def get_tasks(request: Request,
+                    session: AsyncSession = Depends(get_async_session)):
+    try:
+        user_id = get_user_id_from_token(request)
+    except Exception as e:
+        print(e)
+        return "403 FORBIDDEN"
+
+    query = select(task).where(or_(task.c.user_executor_id == user_id,task.c.user_creator_id == user_id))
+    result_raw = await session.execute(query)
+    result = result_raw.fetchall()
+    await session.close()
+    if result is None:
+        return "404 Not found"
+    response = []
+    for tup in result:
+        data = {}
+        data["id"] = tup[0]
+        data["title"] = tup[1]
+        data["description"] = tup[2]
+        data["start_timestamp"] = tup[3]
+        data["user_creator_id"] = tup[4]
+        data["user_executor_id"] = tup[5]
+        data["status_id"] = tup[6]
+        data["board_id"] = tup[7]
+        data["parent_task_id"] = tup[8]
+        response.append(data)
+
+    return response
 class Status(BaseModel):
     status: str
 
@@ -178,3 +230,45 @@ async def get_statuses(request: Request,
         }
         statuses.append(status_data)
     return statuses
+
+async def add_to_allowed_users(session: AsyncSession, task_id: int, user_id: int):
+    query = select(task).where(task.c.id == task_id)
+    result = await query_execute(query, session)
+
+    list_allowed_users: list = result.fetchone()[9]
+    print(list_allowed_users)
+    list_allowed_users.append(user_id)
+    query = update(task).where(task.c.id == task_id).values(allowed_to_visible_user_ids = list_allowed_users)
+    await query_execute(query, session)
+
+
+async def remove_from_allowed_users(session: AsyncSession, task_id: int, user_id: int):
+    query = select(task).where(task.c.id == task_id)
+    result = await query_execute(query, session)
+
+    list_allowed_users: list = result.fetchone()[9]
+    print(list_allowed_users)
+    try:
+        list_allowed_users.remove(user_id)
+    except Exception as e:
+        ...
+    query = update(task).where(task.c.id == task_id).values(allowed_to_visible_user_ids = list_allowed_users)
+    await query_execute(query, session)
+
+@router.get("/{user_name}/task/{task_id}/add/user/{user_id}")
+async def add_user_to_visible(request: Request,
+                        user_name: str,
+                        user_id: int,
+                        task_id: int,
+                        session: AsyncSession = Depends(get_async_session)):
+    await add_to_allowed_users(session, task_id, user_id)
+    return "All ok"
+
+@router.get("/{user_name}/task/{task_id}/remove/user/{user_id}")
+async def remove_user_from_visible(request: Request,
+                        user_name: str,
+                        user_id: int,
+                        task_id: int,
+                        session: AsyncSession = Depends(get_async_session)):
+    await remove_from_allowed_users(session, task_id, user_id)
+    return "All ok!"
